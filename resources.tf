d6ab6153b7f6406411a83286f249c3589a1ef1c7
sandeepchakka <sapsec2006@gmail.com>
2017-09-27 14:10:36 -0400

    Add existing file

---
+++ resources.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+
+resource "aws_security_group" "ForBalancer" {
+name        = "For_Balancer"
+ description = "A security group that will apply to the balancer"
+ vpc_id      = "${var.myvpcid}"
+
+
+# Allow HTTP from anywhere
+ ingress {
+from_port   = 80
+   to_port     = 80
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+
+module "balancer1" {
+source = "../tf-module"
+ modname   = "Balancer1"
+ SecGroupId = "${aws_security_group.ForBalancer.id}"
+}
+
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "us-east-1"
+}
+variable myvpcid {
+default = "vpc-37029151"
+ description = "you can choose the default vpc of the chosen region"
+}
